236ec8384b69f97bc8e38920c50bbee7f6bd4137
ilmoi <iljamoi@protonmail.com>
2021-11-12 20:59:10 +0200

    added ga

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,11 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import './index.css';
+import VueGtag from 'vue-gtag';
 
-createApp(App).use(router).mount('#app');
+createApp(App)
+  .use(router)
+  .use(VueGtag, {
+    config: { id: 'G-6MN98MZZPL' },
+  })
+  .mount('#app');
