82014cff3b55cb6856d872533d1354ba7c027950
ilmoi <iljamoi@protonmail.com>
2021-12-10 08:47:28 +0200

    fix typo

--- src/components/content/tooltip/ContentTooltipJSON.vue
+++ src/components/content/tooltip/ContentTooltipJSON.vue
@@ -3,7 +3,7 @@
     <p>The JSON you're looking at is custom-built and composed of a number of things:</p>
     <div class="lists">
       <ul class="mt-5 ml-4 nes-list is-disc">
-        <li>Address (SPL token account curently holding the NFT)</li>
+        <li>Address (SPL token account currently holding the NFT)</li>
         <li>Mint address (on Solana each NFT has its own mint - unlike on Ethereum)</li>
         <li>Full SPL Token info</li>
         <li>Full SPL Mint info</li>
