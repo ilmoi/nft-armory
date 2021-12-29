2344eb767458f880fd810044f0c2847a3403fab3
Karthik Senthil <karthiksenthil@Prushas-MacBook-Air.local>
2021-12-28 22:53:48 -0500

    cleaning up code / removing console.log

--- src/components/TicketDetail.vue
+++ src/components/TicketDetail.vue
@@ -12,7 +12,7 @@ import { PublicKey } from '@solana/web3.js';
 
 import useWallet from '@/composables/wallet';
 import useError from '@/composables/error';
-import { INFT, INFTParams } from '@/common/helpers/types';
+import { INFT } from '@/common/helpers/types';
 import { useRoute } from 'vue-router';
 import NFTViewCard from '@/components/NFTViewCard.vue';
 import { NFTGet } from '@/common/NFTget';
@@ -36,46 +36,26 @@ export default defineComponent({
     });
     setCluster(Cluster.Devnet);
 
-    console.log("HERE!");
-
     const { isConnected, getWallet, getWalletAddress } = useWallet();
     const { error, clearError, setError } = useError();
-
-   //const ticketID = ref<string | null>(null);
-
     const ticket = ref<INFT | null>(null);
     const ticketID = ref<string | null>(null);
-
-   // var fetchedTickets = ref<INFT[]>([]); 
-
-        console.log("HERE2!");
-
-    const chosenMethod = ref<string>('ticketID');
-
-
-    //console.log("TICKET ID", ticketID);
-
+    
     const fetchTicket = async (ticketID:string) => {
-        console.log("in fetch ticket with ticketID!", ticketID);
-
        if (ticketID != null) {
             try {
                 await NFTGet({ mint: new PublicKey(ticketID) })
                 .then((fetchedNFT) => {
-                    console.log(fetchedNFT);
                     [ticket.value] = fetchedNFT;
                 });          
             } catch (e) {
-                console.log("error", e);
+                console.log("something went wrong when fetching the ticket", e);
                 return null;    
             }
-
         } 
-       
     };
-        console.log("HERE3!");
 
-    // --------------------------------------- sharing links
+    //grabbing ticketID from URL
     onMounted(() => {
       const route = useRoute();
       const {
@@ -86,13 +66,6 @@ export default defineComponent({
           ticketID.value = goTicketID as any as string;
           fetchTicket(ticketID.value);
       }
-
-      /* console.log("route params", route.params);
-            console.log("route params", route.params.ticketID);
-
-      console.log("ticketID", goTicketID);
-      */
-
     });
 
      return {
