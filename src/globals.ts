fd2b484a8583c1f0345b226adb46863ddfe53d47
Karthik Senthil <karthiksenthil@Prushas-MacBook-Air.local>
2022-01-28 13:57:27 -0500

    testing again

--- src/globals.ts
+++ src/globals.ts
@@ -8,10 +8,10 @@ export const ERR_NO_NFTS = new Error('No NFTs Found:( Are you on the right netwo
 
 // ---------------------------------------  defaults
 
-console.log('process: ', process.env.SOLANA_ENV);
+console.log('process: ', `process.env.SOLANA_ENV`);
 
 export const DEFAULTS = {
-  CLUSTER: process.env.SOLANA_ENV ? process.env.SOLANA_ENV : 'devnet',
+  CLUSTER: `process.env.SOLANA_ENV` ? `process.env.SOLANA_ENV` : 'devnet',
 
   OWNER: 'AEahaRpDFzg74t7NtWoruabo2fPJQjKFM9kQJNjH7obK',
   CREATOR: '9px36ZsECEdSbNAobezC77Wr9BfACenRN1W8X7AUuWAb',
